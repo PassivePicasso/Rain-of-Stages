b8e65867d9ad58967855b322a621465307b9395f
James LaPenn <james.lapenn@gmail.com>
2020-09-08 15:34:08 -0400

    lots of progress

---
+++ Assets/LocalDevelopment/Scripts/AssignAmbientEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RainOfStages.Assets.LocalDevelopment.Scripts
+{
+    [RequireComponent(typeof(AkAmbient))]
+    public class AssignAmbientEvent : MonoBehaviour
+    {
+        public string EventId;
+        // Use this for initialization
+        void Awake()
+        {
+            var wwiseEvent = Resources.Load<WwiseEventReference>($"event/{EventId}");
+            var volume = GetComponent<AkAmbient>();
+            volume.data.WwiseObjectReference = wwiseEvent;
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/LocalDevelopment/Scripts/DirectionSchedule.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PassivePicasso.RainOfStages.Variants.Behaviors
+{
+    public class DirectionSchedule : MonoBehaviour
+    {
+        public enum SchedulerState { Holding, Transitioning }
+
+        public float holdTime;
+        public float transitionTime;
+        public Vector3[] Directions;
+
+        private float elapsedTime;
+        private int directionIndex;
+        private SchedulerState state;
+
+        // Update is called once per frame
+        void Update()
+        {
+            switch (state)
+            {
+                case SchedulerState.Holding:
+                    if (elapsedTime < holdTime) elapsedTime += Time.deltaTime;
+                    else
+                    {
+                        state = SchedulerState.Transitioning;
+                        elapsedTime = 0;
+                    }
+                    break;
+                case SchedulerState.Transitioning:
+                    if (elapsedTime > transitionTime)
+                    {
+                        state = SchedulerState.Holding;
+                        elapsedTime = 0;
+                        directionIndex++;
+                    }
+                    else
+                    {
+                        elapsedTime += Time.deltaTime;
+
+                        var curDir = transform.forward;
+                        var nextDirI = directionIndex % Directions.Length;
+                        var nextDir = Directions[nextDirI];
+                        var angle = Vector3.Angle(curDir, nextDir);
+                        var rads = Mathf.Deg2Rad * angle;
+                        var radiansPerSecond = (rads / transitionTime) * Time.deltaTime;
+                        transform.forward = Vector3.RotateTowards(curDir, nextDir, radiansPerSecond, 1);
+                    }
+                    break;
+            }
+        }
+    }
+}
