f4fa0abcd5445bb4a3ff7e94291bcddf20797e0b
PassivePicasso <james.lapenn@gmail.com>
2021-04-03 11:15:54 -0400

    Hide the assetbundle inspector

--- Assets/RainOfStages/Editor/AssetBundleInspector/AssetBundleInspector.cs
+++ Assets/RainOfStages/Editor/AssetBundleInspector/AssetBundleInspector.cs
@@ -7,98 +7,105 @@ using System.Text;
 using System;
 using ThunderKit.Core.Editor.Windows;
 
-public class AssetBundleInspector : TemplatedWindow
+namespace PassivePicasso.RainOfStages
 {
-    [MenuItem("Tools/Rain of Stages/AssetBundleInspector")]
-    public static void ShowExample()
+    using static ThunderKit.Core.UIElements.TemplateHelpers;
+    public class AssetBundleInspector : TemplatedWindow
     {
-        AssetBundleInspector wnd = GetWindow<AssetBundleInspector>();
-        wnd.titleContent = new GUIContent("AssetBundleInspector");
-    }
-
-    [SerializeField] string bundlePath;
-
-    VisualElement AssetList;
-
-    public override void OnEnable()
-    {
-        base.OnEnable();
-
-        var openButton = rootVisualContainer.Q<Button>("OpenButton");
-        AssignClickHandler(openButton, openButton_clicked);
-
-        var unloadButton = rootVisualContainer.Q<Button>("UnloadButton");
-        AssignClickHandler(unloadButton, unloadButton_clicked);
-
-        var scrollElement = new ScrollView();
-        scrollElement.AddToClassList("grow");
-        AssetList = new VisualElement();
-        AssetList.AddToClassList("assetList");
-        scrollElement.Add(AssetList);
-        rootVisualContainer.Add(scrollElement);
-    }
-
-    private void unloadButton_clicked()
-    {
-        AssetBundle.UnloadAllAssetBundles(true);
-    }
-
-    private void openButton_clicked()
-    {
-        AssetList.Clear();
-        bundlePath = EditorUtility.OpenFilePanel("Locate AssetBundle", Directory.GetCurrentDirectory(), "*");
-        Debug.Log($"bundlePath: {bundlePath}");
-
-
-        var loadedBundle = AssetBundle.LoadFromFile(bundlePath);
-        var assets = loadedBundle.GetAllAssetNames();
-
-        var logBuilder = new StringBuilder();
-        logBuilder.AppendLine($"Found {assets.Length} assets;");
-        foreach (var asset in assets)
+        //[MenuItem("Tools/Rain of Stages/AssetBundleInspector")]
+        public static void ShowExample()
         {
-            logBuilder.AppendLine(asset);
-            var button = new Button();
-            button.text = asset;
-            void SelectAsset()
+            AssetBundleInspector wnd = GetWindow<AssetBundleInspector>();
+            wnd.titleContent = new GUIContent("AssetBundleInspector");
+        }
+
+        [SerializeField] string bundlePath;
+
+        VisualElement AssetList;
+
+        public override void OnEnable()
+        {
+            rootVisualElement.Clear();
+            GetTemplateInstance(GetType().Name, rootVisualElement, path => path.StartsWith("Packages/twiner-rainofstages") || path.StartsWith("Assets/RainOfStages"));
+            titleContent = new GUIContent(ObjectNames.NicifyVariableName(GetType().Name), ThunderKitIcon);
+            rootVisualElement.Bind(new SerializedObject(this));
+
+            var openButton = rootVisualElement.Q<Button>("OpenButton");
+            AssignClickHandler(openButton, openButton_clicked);
+
+            var unloadButton = rootVisualElement.Q<Button>("UnloadButton");
+            AssignClickHandler(unloadButton, unloadButton_clicked);
+
+            var scrollElement = new ScrollView();
+            scrollElement.AddToClassList("grow");
+            AssetList = new VisualElement();
+            AssetList.AddToClassList("assetList");
+            scrollElement.Add(AssetList);
+            rootVisualElement.Add(scrollElement);
+        }
+
+        private void unloadButton_clicked()
+        {
+            AssetBundle.UnloadAllAssetBundles(true);
+        }
+
+        private void openButton_clicked()
+        {
+            AssetList.Clear();
+            bundlePath = EditorUtility.OpenFilePanel("Locate AssetBundle", Directory.GetCurrentDirectory(), "*");
+            Debug.Log($"bundlePath: {bundlePath}");
+
+
+            var loadedBundle = AssetBundle.LoadFromFile(bundlePath);
+            var assets = loadedBundle.GetAllAssetNames();
+
+            var logBuilder = new StringBuilder();
+            logBuilder.AppendLine($"Found {assets.Length} assets;");
+            foreach (var asset in assets)
             {
-                var locallyLoadedBundle = AssetBundle.LoadFromFile(bundlePath);
-                var loadedAsset = locallyLoadedBundle.LoadAsset(asset);
-                if(AssetDatabase.TryGetGUIDAndLocalFileIdentifier(loadedAsset.GetInstanceID(), out string guid, out long fileId))
-                    Debug.Log($"Loaded GUID and LocalFileId: {{fileId: {fileId}, guid: {guid}, type: ???}}");
+                logBuilder.AppendLine(asset);
+                var button = new Button();
+                button.text = asset;
+                void SelectAsset()
+                {
+                    var locallyLoadedBundle = AssetBundle.LoadFromFile(bundlePath);
+                    var loadedAsset = locallyLoadedBundle.LoadAsset(asset);
+                    if (AssetDatabase.TryGetGUIDAndLocalFileIdentifier(loadedAsset.GetInstanceID(), out string guid, out long fileId))
+                        Debug.Log($"Loaded GUID and LocalFileId: {{fileId: {fileId}, guid: {guid}, type: ???}}");
 
-                var clonedAsset = Instantiate(loadedAsset);
+                    var clonedAsset = Instantiate(loadedAsset);
 
-                if (AssetDatabase.TryGetGUIDAndLocalFileIdentifier(clonedAsset.GetInstanceID(), out guid, out fileId))
-                    Debug.Log($"Cloned GUID and LocalFileId: {{fileId: {fileId}, guid: {guid}, type: ???}}");
+                    if (AssetDatabase.TryGetGUIDAndLocalFileIdentifier(clonedAsset.GetInstanceID(), out guid, out fileId))
+                        Debug.Log($"Cloned GUID and LocalFileId: {{fileId: {fileId}, guid: {guid}, type: ???}}");
 
-                var newAssetPath = $"Assets/{locallyLoadedBundle.name}/{asset}";
-                locallyLoadedBundle.Unload(true);
+                    var newAssetPath = $"Assets/{locallyLoadedBundle.name}/{asset}";
+                    locallyLoadedBundle.Unload(true);
 
-                Directory.CreateDirectory(Path.GetDirectoryName(newAssetPath));
-                AssetDatabase.CreateAsset(clonedAsset, newAssetPath);
-                //AssetDatabase.CopyAsset(AssetDatabase.GetAssetPath(loadedAsset), newAssetPath);
-                Selection.activeObject = AssetDatabase.LoadAssetAtPath(newAssetPath, clonedAsset.GetType());
-                EditorGUIUtility.PingObject(Selection.activeObject);
+                    Directory.CreateDirectory(Path.GetDirectoryName(newAssetPath));
+                    AssetDatabase.CreateAsset(clonedAsset, newAssetPath);
+                    //AssetDatabase.CopyAsset(AssetDatabase.GetAssetPath(loadedAsset), newAssetPath);
+                    Selection.activeObject = AssetDatabase.LoadAssetAtPath(newAssetPath, clonedAsset.GetType());
+                    EditorGUIUtility.PingObject(Selection.activeObject);
+                }
+
+                AssignClickHandler(button, SelectAsset);
+                AssetList.Add(button);
             }
-            
-            AssignClickHandler(button, SelectAsset);
-            AssetList.Add(button);
+            loadedBundle.Unload(true);
+
+            Debug.Log(logBuilder.ToString());
         }
-        loadedBundle.Unload(true);
 
-        Debug.Log(logBuilder.ToString());
-    }
-
-    private void AssignClickHandler(Button button, Action action)
-    {
-        if (button.clickable != null)
+        private void AssignClickHandler(Button button, Action action)
         {
-            button.clickable.clicked -= action;
-            button.clickable.clicked += action;
+            if (button.clickable != null)
+            {
+                button.clickable.clicked -= action;
+                button.clickable.clicked += action;
+            }
+            else
+                button.clickable = new Clickable(action);
         }
-        else
-            button.clickable = new Clickable(action);
-    }
 
+    }
 }
